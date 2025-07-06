058166fec42337519d3ea6e6899a94ac5e4dd92a
jobayer-rahman <jobayer_rahman@yahoo.com>
2025-07-07 02:18:12 +0600

    Added a more efficient method to check a number is even or odd.

--- Logic Building Problems/1_solution_even_or_odd.py
+++ Logic Building Problems/1_solution_even_or_odd.py
@@ -1,9 +1,24 @@
-def even_or_odd(number):
+def check_even_or_odd_with_remainder(number):
     if number % 2 == 0:
-        print(True)
+        return True
     else:
-        print(False)
+        return False
+    
+def check_even_or_odd_with_bitwise(number):
+    """
+        This method is more efficient because it does not do any operation,
+        just check the bit of a number. For the even number the bit is always
+        0 and for the odd number the bit is 1. That's it.
+    """
+    if number & 1 == 0:
+        return True
+    else:
+        return False
         
 
-number = int(input())
-even_or_odd(number=number)
+if __name__ == '__main__':
+    number = int(input())
+    if check_even_or_odd_with_bitwise(number=number):
+        print("true")
+    else:
+        print("false")
